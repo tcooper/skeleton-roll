a551e8884e97b785dcd135b2cd14432e0f7d85f9
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 16:01:11 -0800

    Added more templating

--- src/skeleton/version.mk
+++ src/skeleton/version.mk
@@ -1,9 +1,15 @@
-PKGROOT		= /opt/skeleton
-NAME		= skeleton
-VERSION		= 1.0
-RELEASE		= 1
-TARBALL_POSTFIX	= tgz
+PKGROOT            = /opt/skeleton
+NAME               = skeleton
+VERSION            = 1.0
+RELEASE            = 1
+TARBALL_POSTFIX    = tgz
 
-SRC_SUBDIR	= skeleton
+SRC_SUBDIR         = skeleton
 
-TGZ_PKGS = $(NAME)-$(VERSION).$(TARBALL_POSTFIX)
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
